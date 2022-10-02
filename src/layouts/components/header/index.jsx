14a110d3f049db2c747a62789be385fc5de20f9e
swarosky44 <swarosky44@gmail.com>
2022-10-03 01:55:24 +0800

    feat:调整;

--- src/layouts/components/header/index.jsx
+++ src/layouts/components/header/index.jsx
@@ -14,7 +14,7 @@ export default () => {
         {[
           { name: 'Home', path: '/home' },
           { name: 'Adventure3', path: '/adventure3' },
-          { name: 'Native Score', path: '/native' },
+          // { name: 'Native Score', path: '/native' },
         ].map((m, i) => (
           <Link to={m.path} key={i}>
             <span className={styles.menu}>{m.name}</span>
